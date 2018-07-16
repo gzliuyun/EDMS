b50673243b89470d1b5b415321a0cd4fb5a13b26
zyk930 <zhouyk@buaa.edu.cn>
2018-07-16 16:34:52 +0800

    添加已经爬取学校及学院

--- Crawler/ExpertPortrait/ExpertPortrait/items.py
+++ Crawler/ExpertPortrait/ExpertPortrait/items.py
@@ -44,13 +44,5 @@ class paper(scrapy.Item):
     p_author4 = scrapy.Field()
     p_author5 = scrapy.Field()
 
-class target(scrapy.Item):
-    flag = scrapy.Field()
-    university = scrapy.Field()
-
-class all(scrapy.Item):
-    total = scrapy.Field()
-    page_count = scrapy.Field()
-    col_count = scrapy.Field()
 
     pass
