9ed41216eb1b17643c090331fb28487be854ce16
bellwind <bellwind@foxmail.com>
2019-02-27 16:56:08 +0800

    update baike

--- Crawler/ExpertBaike/ExpertBaike/items.py
+++ Crawler/ExpertBaike/ExpertBaike/items.py
@@ -7,8 +7,18 @@
 
 import scrapy
 
-
 class ExpertbaikeItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class ExpertInfoItem:
+    def __init__(self):
+        self.school = ""
+        self.name = ""
+        self.id = ""
+
+    def __init__(self, school, name, id):
+        self.school = school
+        self.name = name
+        self.id = id
--- Crawler/ExpertBaike/ExpertBaike/spiders/Baike.py
+++ Crawler/ExpertBaike/ExpertBaike/spiders/Baike.py
@@ -2,40 +2,49 @@
 import scrapy
 from scrapy.selector import Selector
 from scrapy import Request
+from ExpertBaike.items import ExpertInfoItem
 import re
 from urllib.parse import unquote
 
-
 class BaikeSpider(scrapy.Spider):
     name = 'Baike'
-    allowed_domains = ['www.baidu.com']
+    allowed_domains = ['baike.baidu.com']
     start_urls = []
+    keyword_dict = dict()
 
     def __init__(self):
         with open('data.txt') as f:
             lines = f.readlines()
             for line in lines:
                 words = line.rstrip('\n').split(' ')
+                ei = ExpertInfoItem(words[0], words[1], words[2])
+                keyword = words[0] + "+" + words[1]
                 url = "https://baike.baidu.com/search/none?word=" + \
-                      words[0] + "+" + words[1] + \
+                       keyword + \
                       "&pn=0&rn=10&enc=utf8"
-                # print(url)
                 self.start_urls.append(url)
+                self.keyword_dict[keyword] = ei
 
     def parse(self, response):
         url = response.url
-        # print(url)
+
+        keyword = unquote(url.\
+            lstrip("https://baike.baidu.com/search/none?word=").\
+            rstrip("&pn=0&rn=10&enc=utf8"))
+
+        # print("####: " + keyword )
+        # print(self.keyword_dict[keyword].id)
+
         sel = Selector(response)
-        print("-------------")
         contents = sel.xpath('//*[@id="body_wrapper"]/div[1]/dl/dd[1]/a').extract()
         for con in contents:
-            baike_url = re.sub(r"(https://.+?)/.*", lambda x: x.group(1), con)[30:]
-            print(baike_url, baike_url[0])
-            if baike_url[0] == 'h':
-                yield Request(baike_url, callback=self.parse2)
+            if con.find("result-title"):
+                next_url = con[30:].split(' ')[0].rstrip('"')
+                yield Request(next_url, callback=self.parse2, meta={'keyword': keyword})
 
     def parse2(self, response):
-        print("~~~~~~~")
         url = response.url
+        keyword = response.meta['keyword']
         print(url)
+        print(keyword)
 
